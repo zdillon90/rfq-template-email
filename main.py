3adffcd2996ea243674fdf0487471b1154a94ce1
Zach Dillon <zdillon90@gmail.com>
2021-12-28 19:49:48 +0000

    trying the google api path

--- main.py
+++ main.py
@@ -17,23 +17,32 @@ mail.login(sender_email, password)
 mail.list()
 mail.select("inbox")
 
-message = """
-Account: 
-Model(s): 
-Directional Quote [D] or Production [P]: 
-# of Models to Quote (Quantity): 
-Quantities to quote of each Model: 
-Technology: 
-Material: 
-Finish: 
-Expected Lead Time: 
-Additional Instructions/Comments: 
-End Use:
-"""
+result, data = mail.search(None, "ALL")
+ids = data[0] # data is a list.
+id_list = ids.split() # ids is a space separated string
+latest_email_id = id_list[-1] # get the latest
+result, data = mail.fetch(latest_email_id, "(RFC822)") # fetch the email body (RFC822) for the given ID
+raw_email = data[0][1] # here's the body, which is raw text of the whole email
+# including headers and alternate payloads
+print(ids)
 
-context = ssl.create_default_context()
+# message = """
+# Account: 
+# Model(s): 
+# Directional Quote [D] or Production [P]: 
+# # of Models to Quote (Quantity): 
+# Quantities to quote of each Model: 
+# Technology: 
+# Material: 
+# Finish: 
+# Expected Lead Time: 
+# Additional Instructions/Comments: 
+# End Use:
+# """
 
-with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-    server.login("rfq.template.test@gmail.com", password)
-    server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message)
+# context = ssl.create_default_context()
+
+# with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+#     server.login("rfq.template.test@gmail.com", password)
+#     server.login(sender_email, password)
+#     server.sendmail(sender_email, receiver_email, message)
